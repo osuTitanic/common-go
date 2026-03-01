d372c74c5f7a08ee05e394ed75778ab508a0a7e4
Lekuru <contact@lekuru.xyz>
2026-03-01 14:03:46 +0100

    Make beatmapset ratings & favourite columns read-only

--- schemas/beatmaps.go
+++ schemas/beatmaps.go
@@ -34,10 +34,10 @@ type Beatmapset struct {
 	LastUpdate         time.Time               `gorm:"column:last_updated;autoCreateTime"`
 	AddedAt            *time.Time              `gorm:"column:added_at;autoCreateTime"`
 	TotalPlaycount     int64                   `gorm:"column:total_playcount;default:0"`
-	MaxDiff            float64                 `gorm:"column:max_diff;default:0.0"`
-	RatingAverage      float64                 `gorm:"column:rating_average;default:0.0"`
-	RatingCount        int                     `gorm:"column:rating_count;default:0"`
-	FavouriteCount     int                     `gorm:"column:favourite_count;default:0"`
+	MaxDiff            float64                 `gorm:"column:max_diff;default:0.0;->"`
+	RatingAverage      float64                 `gorm:"column:rating_average;default:0.0;->"`
+	RatingCount        int                     `gorm:"column:rating_count;default:0;->"`
+	FavouriteCount     int                     `gorm:"column:favourite_count;default:0;->"`
 	OszFilesize        int                     `gorm:"column:osz_filesize;default:0"`
 	OszFilesizeNovideo int                     `gorm:"column:osz_filesize_novideo;default:0"`
 	LanguageId         int                     `gorm:"column:language_id;default:1"`
